3ba7dd5e866b625a61775234497c8150e7bc5c32
Aline Soares <aliinanda@example.com>
2025-02-01 23:53:51 -0300

    main1

---
+++ main1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b;
+
+    printf("digite um valor para a:");
+    scanf("%d", &a);
+    getchar();
+
+     printf("digite um valor para b:");
+    scanf("%d", &b);
+    getchar();
+
+
+    if(a < b){
+        printf("a menor que b\n");
+    }
+    else if(b < a){
+        printf("b menor que a\n");
+    }
+    else{
+        printf("a e igual a b");
+    }
+}
