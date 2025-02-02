1fe7047164ed970bb2d2682fc4811646dcd84aaa
Aline Soares <aliinanda@example.com>
2025-02-02 19:02:44 -0300

    3

--- main3.c
+++ main3.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+int main(){
+
+    int op = 1;
+
+    do{
+        printf("digite a opcao : 1 - sim, 2 - nao\n");
+        scanf("%d", &op);
+        getchar();
+    }while(op < 1 || op > 2);
+
+    return 0;
+}
