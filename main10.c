268214c7fa2ca215091a61087e21f7341fb5395b
Aline Soares <aliinanda@example.com>
2025-02-06 16:47:17 -0300

    a

--- main10.c
+++ main10.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int* insere(int* v1, int valor, int tam, int pos){
+
+    if(pos == tam){// limite
+        int* v2 = (*int)realloc(v1,sizeof(int) * tam * 2);
+        v2[pos] = valor;
+        return v2;
+    }
+    v1[pos] = valor;
+    return v1;
+}
 
 int main(){
 
