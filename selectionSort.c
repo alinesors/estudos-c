f175919a2b8add5e1505691e84ad09e1f290a251
Aline Soares <aliinanda@example.com>
2025-02-20 19:49:37 -0300

    add

---
+++ selectionSort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void selectionSortCrescente(int *vet, int n) {
+    int i, j, min, aux;
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
+
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (vet[j] < vet[min]) {
+                min = j;
+            }
+        }
+
+        if (min != i) {
+            aux = vet[i];
+            vet[i] = vet[min];
+            vet[min] = aux;
+
+            for (j = 0; j < n; j++) {  
+                printf("%d ", vet[j]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main() {
+    int *vet = NULL;
+    int contador = 0, num;
+
+    while (scanf("%d", &num) != EOF) {
+        int *aux = realloc(vet, (contador + 1) * sizeof(int));
+        if (aux == NULL) {
+            free(vet);
+            return 1;
+        }
+        vet = aux;
+        vet[contador++] = num;
+    }
+
+    if (contador > 0) {
+        selectionSortCrescente(vet, contador);
+    }
+
+    free(vet);
+    return 0;
+}
